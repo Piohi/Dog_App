75617cafe0b6d8a2c79d09ba505eea8713950e8f
Piohi <box1234567b@gmail.com>
2023-08-03 14:40:34 +0300

    Initial commit

---
+++ Dog_App/Dog_App/Dog_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dog_AppApp.swift
+//  Dog_App
+//
+//  Created by Anton Godunov on 03.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct Dog_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
